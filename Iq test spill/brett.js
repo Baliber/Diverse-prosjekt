071f46ec1be390db4fa18416f2128e7db75829f8
Balibber <88585231+Baliber@users.noreply.github.com>
2025-02-21 13:35:17 +0100

    ...

--- Iq test spill/brett.js
+++ Iq test spill/brett.js
@@ -1,19 +1,18 @@
 
 let anTallrakkerSlider = document.getElementById("anTallrakkerSlider")
-/* let antallrekker = document.getElementById("sliderValue").value */
+/* let antallRekker = document.getElementById("sliderValue").value */
 let antall = 0
 let høyde = 100
 let brett = document.getElementById("brett")
 let distanseFraTop = 0
 let distanseFraLeft = 500
-function lagBrett(antallrekker){
+function lagBrett(antallRekker){
     antall = 0
     høyde = 100
     distanseFraTop = 0
     distanseFraLeft = 500
     LeftØkning = 0
-    console.log(antallrekker)
-    for(let i = 0; i < antallrekker+1; i++){
+    for(let i = 0; i < antallRekker+1; i++){
         for(let j = 0; j < antall; j++)
         {
             if(j>0){
@@ -41,15 +40,21 @@ function lagBrett(antallrekker){
             brikke.style.width = "30px";
             brikke.style.height = "30px";
             brikke.style.backgroundColor = "green";
+            if (i == 1 && j ==0){
+                brikke.style.backgroundColor = "red";
+                brikke.setAttribute("class", "rødBrikke")
+            }
             brikke.style.borderRadius = "50%";
             brikke.style.position = "absolute";
             brikke.style.top = "50%";
             brikke.style.left = "50%";
             brikke.style.transform = "translate(-50%, -50%)";
-            
+            brikke.setAttribute("id", i +" " +parseInt(j+1))
+            /* console.log(brikke.id) */
             sirkel.appendChild(brikke);
             div.appendChild(sirkel);
             brett.appendChild(div);
+
             /* const rect = div.getBoundingClientRect();
             console.log(`Top: ${rect.top}, Left: ${rect.left}`); */
 
@@ -61,11 +66,12 @@ function lagBrett(antallrekker){
 
         "Math.tan(30*(Math.PI/180))*høyde"
     }
+    console.log(antallRekker+" "+parseInt(antallRekker/2 +1))
+    document.getElementById(String(antallRekker+" "+parseInt(antallRekker/2 + 1))).remove()
 }
 lagBrett(parseInt(document.getElementById("sliderValue").value))
 
 anTallrakkerSlider.addEventListener("input", function() {
-
     document.querySelectorAll(".rute").forEach(div => div.remove());
     lagBrett(parseInt(document.getElementById("sliderValue").value)); // Call your function
   });
